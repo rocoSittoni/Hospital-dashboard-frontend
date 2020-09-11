c79c3a36c3d2efa746d13e689a42b1e1b31b45a4
rocoSittoni <roco.sittoni@gmail.com>
2020-09-11 12:18:22 -0300

    servicios y rutas

---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+
+  public menu: any[] = [
+    {
+      title: 'Main',
+      icon: 'mdi mdi-gauge',
+      submenu: [
+        { title: 'Dashboard', url: '/dashboard'},
+        { title: 'ProgressBar', url: '/progress'},
+        { title: 'Graficas', url: '/grafica1'}
+      ]
+    }
+  ];
+  constructor() { }
+}
